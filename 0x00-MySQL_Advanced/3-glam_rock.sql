e167a99afadb381ac212b11c04cbf5152643c55f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 17:40:01 +0100

    Fix 1 for task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
 -- Task 3 for MySQL Advanced
 -- SQL script that lists all bands with Glam rock as their main style
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-GROUP BY band_name
 ORDER BY lifespan DESC;
