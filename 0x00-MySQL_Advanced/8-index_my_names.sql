04ec3fe24d27b241c3ce69f383d7c21f0243aff7
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 19:11:26 +0100

    Fix 1 for task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,4 @@
 -- Task 8 for MySQL Advanced
 -- Create an index idx_name_first on the table names
 -- and the first letter of name
-DROP INDEX IF EXISTS idx_name_first_score;
-CREATE INDEX idx_name_first_score
-ON names (LEFT(name), score);
+CREATE INDEX idx_name_first_score ON names (name(1));
