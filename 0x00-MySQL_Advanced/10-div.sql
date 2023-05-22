05f39d678bbc53f0bddba99591a2747825188bd2
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 19:22:09 +0100

    Added file for task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Task 10 for MySQL Advanced
+-- SQL script that creates a function SafeDiv
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+-- Edit Function body below
+BEGIN
+	DECLARE div FLOAT DEFAULT 0.0;
+	IF b != 0 THEN
+		SET div = a / b;
+	END IF;
+	RETURN div;
+END $$
+DELIMITER ;
