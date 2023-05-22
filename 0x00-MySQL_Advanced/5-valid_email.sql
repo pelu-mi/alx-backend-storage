58df5af1687704d559222e2972a6dc14d05f400d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 18:09:25 +0100

    Added file for task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Task 5 for MySQL Advanced
+-- SQL script: Trigger that resets the attribute valid_email
+-- only when the email has been changed
+DROP TRIGGER IF EXISTS valid_email_reset;
+DELIMITER $$
+CREATE TRIGGER valid_email_reset
+AFTER UPDATE ON users
+FOR EACH ROW
+-- Edit Trigger body below
+BEGIN
+	IF OLD.email != NEW.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END $$
+DELIMITER ;
