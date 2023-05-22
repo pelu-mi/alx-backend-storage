a08c37eeae039fe8972c919ac82ae0d8d1098fe0
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 18:40:12 +0100

    Fix 1 for task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,11 +5,12 @@ DELIMITER $$
 CREATE PROCEDURE AddBonus (IN user_id INT, INOUT project_name VARCHAR(255), IN score INT)
 -- Edit Procedure body below
 BEGIN
+	DECLARE proj_id INT DEFAULT 0;
 	IF SELECT EXISTS(SELECT * FROM projects WHERE name = project_name) THEN
 		INSERT INTO projects (name)
 		VALUES (project_name);
 	END IF;
-	SELECT @proj_id := id FROM projects WHERE name = project_name;
+	SELECT id INTO proj_id FROM projects WHERE name = project_name;
 	INSERT INTO corrections (user_id, project_id, score)
 	VALUES (user_id, @proj_id, score);
 END $$
