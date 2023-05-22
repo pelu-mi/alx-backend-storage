1656467cb5d2c0651a21e11dced4ab5d3fae50f6
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 19:01:47 +0100

    Fix 1 for task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,9 +7,9 @@ CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT DEFAULT 0.0;
 	SELECT AVG(score) INTO avg_score FROM corrections
-	WHERE user_id = user_id;
+	WHERE corrections.user_id = user_id;
 	UPDATE users
-	SET average_score = avg_score
+	SET users.average_score = avg_score
 	WHERE users.id = user_id;
 END $$
 DELIMITER ;
