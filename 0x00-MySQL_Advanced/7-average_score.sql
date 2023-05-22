af5fabc51a6aa413e649dfd3005a23a1811055f1
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 18:58:02 +0100

    Added file for task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- Task 7 for MySQL Advanced
+-- Stored procedure that computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+-- Edit Procedure body below
+BEGIN
+	DECLARE avg_score FLOAT DEFAULT 0.0;
+	SELECT AVG(score) INTO avg_score FROM corrections
+	WHERE user_id = user_id;
+	UPDATE users
+	SET average_score = avg_score
+	WHERE users.id = user_id;
+END $$
+DELIMITER ;
