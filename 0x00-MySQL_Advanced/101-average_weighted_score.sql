444507a8ef765789cfd1c53a319264c7a3e917dc
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-22 20:17:17 +0100

    Fix 1 for task 13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,40 +5,14 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers
 -- Edit Procedure body below
 BEGIN
-	DECLARE length INT DEFAULT 0;
-	DECLARE counter INT DEFAULT 0;
-	DECLARE total_score INT DEFAULT 0;
-	DECLARE total_weight INT DEFAULT 0;
-	DECLARE user_id INT DEFAULT 0;
-
-	SELECT COUNT(*) FROM users INTO length;
-        SET counter = 0;
-        WHILE counter < length DO
-	
-	-- Loop begins here
-	SET user_id = counter;
-	SELECT SUM(corrections.score * projects.weight) INTO total_score
-	FROM corrections
-        INNER JOIN projects
-        ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user_id;
-	
-	SELECT SUM(projects.weight) INTO total_weight
-	FROM corrections
-	INNER JOIN projects
-	ON corrections.project_id = projects.id
-	WHERE corrections.user_id = user_id;
-
-	IF total_weight <= 0 THEN
-		UPDATE users
-		SET users.average_score = 0
-		WHERE users.id = user_id;
-	ELSE
-		UPDATE users
-		SET users.average_score = total_score / total_weight
-		WHERE users.id = user_id;
-	END IF;
-	-- Loop ends here
-	END WHILE;
+	UPDATE users AS U,
+		(SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
+		FROM users AS U
+		JOIN corrections as C ON U.id = C.user_id
+		JOIN projects AS P ON C.project_id = P.id
+		GROUP BY U.id)
+	AS WA
+	SET U.average_score = WA.w_avg
+	WHERE U.id=WA.id;
 END $$
 DELIMITER ;
