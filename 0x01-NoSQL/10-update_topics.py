d7b989b05e2c6a0ab254a53b34b8ef28dfba0313
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-23 18:45:01 +0100

    Fix 1 for task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,7 +8,7 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """ Change all topics of a document based on the name
     """
-    mongo_collections.update_many(
+    mongo_collection.update_many(
             {"name": name},
             {"$set": {"topics": topics}}
     )
