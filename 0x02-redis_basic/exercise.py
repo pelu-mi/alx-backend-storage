bdf88aad2e5b5cdf13c267edc814856ca3690421
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-29 12:25:00 +0100

    Added changes for task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,10 +3,25 @@
 """
 
 from typing import Any, Union, Callable
+from functools import wraps
 import uuid
 import redis
 
 
+def count_calls(method: Callable) -> Callable:
+    """ Decorator for class Cache
+    """
+    @wraps(method)
+    def for_each_call(self, *args, **kwargs):
+        """ Wrapped function for the decorator
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return for_each_call
+
+@count_calls
 class Cache:
     def __init__(self) -> None:
         """ Initialize the Cache class
@@ -21,7 +36,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable[[Any], Any] = None):
+    def get(self, key: str, fn: Callable[[Any], Any] = None) -> Any:
         """ Get item from db and call fn to convert it from byte to datatype
         """
         value: bytes = self._redis.get(key)
