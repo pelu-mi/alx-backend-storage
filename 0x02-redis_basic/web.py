0f4fe34b588aecb2919b62e93027d335bc50c6d3
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-29 14:10:22 +0100

    Fix 7 for task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,14 +4,42 @@
 
 import requests
 import redis
+
+
+
 cache = redis.Redis()
 
 
-def get_page(url: str) -> str:
-    """ Obtain content of html page and return it
+def url_access_count(method):
+    """ Decorator for get_page
     """
-    cache.set("cached:" + url, 0)
-    cache.incr("count:" + url)
-    cache.setex("cached:" + url, 10, cache.get("cached:" + url))
-    data = requests.get(url)
-    return data.text
+    @wraps(method)
+    def for_each_call(url):
+        """ Wrapper function
+        """
+        key = "cached:" + url
+        cached_value = cache.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+        # Update cache with new content
+        key_count = "count:" + url
+        html_content = method(url)
+
+        cache.incr(key_count)
+        cache.set(key, html_content, ex=10)
+        cache.expire(key, 10)
+        return html_content
+    return for_each_call
+
+
+@url_access_count
+def get_page(url: str) -> str:
+    """ Get the content of a page
+    """
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
