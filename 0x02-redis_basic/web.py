6a92fb9fcca863969ab77959490288d235973398
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-29 14:03:53 +0100

    Fix 6 for task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-""" Module contaning simple tasks for redis project
+""" Module contaning simple redis cache
 """
 
 import requests
 import redis
-
-
 cache = redis.Redis()
 
 
 def get_page(url: str) -> str:
     """ Obtain content of html page and return it
     """
-    cache.set("cached:" + key, 0)
-    cache.incr("count:" + key)
-    cache.setex("cached:" + key, 10, cache.get("cached:" + key))
+    cache.set("cached:" + url, 0)
+    cache.incr("count:" + url)
+    cache.setex("cached:" + url, 10, cache.get("cached:" + url))
     data = requests.get(url)
     return data.text
