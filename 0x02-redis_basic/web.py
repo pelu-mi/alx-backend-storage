14ca63405e0df5dcdc67081d4cbe578e1f1760a1
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-29 13:54:20 +0100

    Fix 4 for task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,11 +8,13 @@ import redis
 
 cache = redis.Redis()
 
+
 def get_page(url: str) -> str:
     """ Obtain content of html page and return it
     """
     key = "count:" + url
+    cache.set("cached:" + key, 0)
     cache.incr(key)
-    cache.setex(key, 10, cache.get(key))
+    cache.setex(key, 10, cache.get("cached:" + key))
     data = requests.get(url)
     return data.text
